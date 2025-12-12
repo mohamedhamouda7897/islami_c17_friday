fb633b38e66b3cb9406c93f9da33702e9346c5b6
Hamouda <mohamed.hamouda7897@gmail.com>
2025-12-12 14:17:32 +0200

    add scaffold

--- lib/home.dart
+++ lib/home.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   static const String routeName = "Home";
 
-
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(body: Column(
 
-    return const Placeholder();
+
+    ));
   }
 }
